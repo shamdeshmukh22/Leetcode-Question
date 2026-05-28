d63a586474d0f283df6d07b5c9bd66009416051b
sham_deshmukh_02 <141163978+shamdeshmukh22@users.noreply.github.com>
2026-05-28 19:35:02 +0530

    Time: 3 ms (50.81%), Space: 46.7 MB (43.77%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+       StringBuilder sb=new StringBuilder(magazine);
+       for(int i=0;i<ransomNote.length();i++){
+        String ch=ransomNote.charAt(i)+"";
+        if(sb.indexOf(ch) != -1){
+           sb.setCharAt(sb.indexOf(ch), '!');
+        }
+        else return false;
+       }
+       return true;
+    
+    }
+
+}
