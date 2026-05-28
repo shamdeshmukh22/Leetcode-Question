d0163eb1672e72732f1f66f3b4e3b1da85495f9f
sham_deshmukh_02 <141163978+shamdeshmukh22@users.noreply.github.com>
2026-05-29 01:33:32 +0530

    Time: 30 ms (83.21%), Space: 42 MB (88.3%) - LeetHub

--- 0866-prime-palindrome/0866-prime-palindrome.java
+++ 0866-prime-palindrome/0866-prime-palindrome.java
@@ -1,27 +1,28 @@
 class Solution {
-    public boolean CheckPrime(long num){
-        if(num==2) return true;
-        if(num%2==0||num<2) return false;
-        for(long i=3;i*i<=num;i+=2){
-            if(num%i==0) return false;
+    public static boolean checkPrime(int n){
+        if(n==2) return true;
+        if(n<2 || n%2==0) return false;
+        for(int i=3;i*i<=n;i+=2){
+            if(n%i==0) return false;
         }
         return true;
     }
-    public boolean checkPalindrome(int n){
-        int rev=0;
-        int prev=n;
-          while(n>0){
+    public static  boolean  checkPalindrome(int n){
+        int prev=n,rev=0;
+        while(n>0){
             rev=rev*10+ n%10;
             n/=10;
-          }
+        }
         return prev==rev;
     }
     public int primePalindrome(int n) {
-        for(int i=n;i<Integer.MAX_VALUE;i++){
-             if(10000000 < i && i < 100000000)
+       for(int i=n;i<Integer.MAX_VALUE;i++){
+         if(10000000 < i && i < 100000000)
                 i = 100000000;
-            if(checkPalindrome(i) && CheckPrime(i) ) return i;
-        }
-         return -1;
+         if(checkPalindrome(i) && checkPrime(i)){
+            return i;
+         }
+       }
+       return -1;
     }
 }
