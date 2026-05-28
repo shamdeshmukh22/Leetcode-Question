4a3549c1846782e742da345069e32756fef34bf9
sham_deshmukh_02 <141163978+shamdeshmukh22@users.noreply.github.com>
2026-05-28 19:35:26 +0530

    Time: 3 ms (50.81%), Space: 46.3 MB (68.04%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
        StringBuilder sb=new StringBuilder(magazine);
        for(int i=0;i<ransomNote.length();i++){
-        String ch=ransomNote.charAt(i)+"";
+        String ch=String.valueOf(ransomNote.charAt(i));
         if(sb.indexOf(ch) != -1){
            sb.setCharAt(sb.indexOf(ch), '!');
         }
