5afedff6853597bb1f710332ff3798e577543c7c
sham_deshmukh_02 <141163978+shamdeshmukh22@users.noreply.github.com>
2026-06-09 11:15:07 +0530

    Time: 0 ms (100%), Space: 42.3 MB (23.01%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int integerReplacement(int num) {
+        long n=num;
+        int count=0;
+       
+         while(n!=1){
+            if(n%2==0){
+                n/=2;
+            }else{
+                 if(n==3 || (n-1)%4==0){
+                    n--;
+                 }
+                 else n++;
+            }
+           count++; 
+         }
+
+        return count;
+    }
+}
