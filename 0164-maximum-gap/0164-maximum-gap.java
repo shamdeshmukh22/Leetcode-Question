e175237df1f739135bf527783e622614063901d7
sham_deshmukh_02 <141163978+shamdeshmukh22@users.noreply.github.com>
2026-06-12 12:34:50 +0530

    Time: 72 ms (5.07%), Space: 95.4 MB (7.88%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.java
+++ 0164-maximum-gap/0164-maximum-gap.java
@@ -1,11 +1,37 @@
 class Solution {
     public int maximumGap(int[] nums) {
         if(nums.length<2) return 0;
-        Arrays.sort(nums);
+        mergeSortAlgorithm(nums);
         int diff=Integer.MIN_VALUE;
         for(int i=1;i<nums.length;i++){
             diff=Math.max(diff,nums[i]-nums[i-1]);
         }
         return diff;
+    }
+    public void mergeSortAlgorithm(int arr[]){
+        int n=arr.length,k=0;
+        if(n==1) return;
+        int a[]=new int[n/2];
+        int b[]=new int[n-n/2];
+       
+        for(int i=0;i<a.length;i++){
+            a[i]=arr[k++];
+        }
+        for(int i=0;i<b.length;i++){
+            b[i]=arr[k++];
+        }
+        mergeSortAlgorithm(a);mergeSortAlgorithm(b);
+        mergeArray(a,b,arr);
+    }
+    public void mergeArray(int a[],int b[],int arr[]){
+        int i=0,j=0,k=0;
+        while(i<a.length && j<b.length){
+            if(a[i]<b[j]){
+                arr[k++]=a[i++];
+            }
+            else arr[k++]=b[j++];
+        }
+        while(i<a.length) arr[k++]=a[i++];
+        while(j<b.length) arr[k++]=b[j++];
     }
 }
