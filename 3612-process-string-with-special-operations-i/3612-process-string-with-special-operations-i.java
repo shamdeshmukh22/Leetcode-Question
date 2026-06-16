279646da00e36f58c1d36380fde17c509db9e324
sham_deshmukh_02 <141163978+shamdeshmukh22@users.noreply.github.com>
2026-06-16 18:01:53 +0530

    Time: 5 ms (20.59%), Space: 55.2 MB (58.82%) - LeetHub

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -1,14 +1,14 @@
 class Solution {
     public String processStr(String s) {
         StringBuilder sb= new StringBuilder("");
+
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
             if(ch>='a' && ch<='z'){
                 sb.append(ch);
             }
             else if(ch=='*' && sb.length()>0){
-                String str=sb.substring(0,sb.length()-1);
-                sb=new StringBuilder(str);
+               sb.deleteCharAt(sb.length() - 1); 
             }
             else if(ch=='#'){
                 String str=sb.toString();
