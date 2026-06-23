09c994958f22376fee4c7b372e31eee625a3a625
sham_deshmukh_02 <141163978+shamdeshmukh22@users.noreply.github.com>
2026-06-23 23:03:34 +0530

    Time: 11 ms (85.26%), Space: 47 MB (89.66%) - LeetHub

---
+++ 3941-password-strength/3941-password-strength.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int passwordStrength(String password) {
+        int strength=0;
+        HashSet<Character>set=new HashSet<>();
+        for(int i=0;i<password.length();i++){
+            char ch=password.charAt(i);
+            if(! set.contains(ch)){
+                if(ch>='a'&& ch<='z')strength+=1;
+               else if(ch>='A'&& ch<='Z')strength+=2;
+               else if(ch>='0'&& ch<='9')strength+=3;
+               else if(CheckSpetiail(ch))strength+=5;
+                set.add(ch);
+            }
+        }
+        return strength;
+    }
+    public static boolean CheckSpetiail(char ch){
+        return ch=='!' || ch=='@' ||  ch=='#' ||ch=='$';
+    }
+}
