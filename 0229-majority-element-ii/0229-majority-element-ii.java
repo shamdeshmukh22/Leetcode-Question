29be1344769847e7c637ec3dcda49d3dfb64fc79
sham_deshmukh_02 <141163978+shamdeshmukh22@users.noreply.github.com>
2026-07-10 17:27:55 +0530

    Time: 14 ms (41.01%), Space: 51.4 MB (88.76%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -4,11 +4,7 @@ class Solution {
       HashMap<Integer,Integer>map=new HashMap<>();
       int count=nums.length/3;
       for(int i=0;i<nums.length;i++){
-           if(map.containsKey(nums[i])){
-            int key=map.get(nums[i])+1;
-            map.put(nums[i],key);
-           }
-           else map.put(nums[i],1);
+           map.put(nums[i],map.getOrDefault(nums[i],0)+1);
       }
       for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
             int element = entry.getKey();
