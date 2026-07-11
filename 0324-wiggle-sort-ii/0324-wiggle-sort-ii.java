f2aa69ac51d44553367e3c0c6e86b040d171354c
sham_deshmukh_02 <141163978+shamdeshmukh22@users.noreply.github.com>
2026-07-11 23:28:39 +0530

    Time: 8 ms (97.31%), Space: 48.7 MB (91.94%) - LeetHub

--- 0324-wiggle-sort-ii/0324-wiggle-sort-ii.java
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.java
@@ -2,8 +2,9 @@ class Solution {
     public void wiggleSort(int[] nums) {
         int copy[]=nums.clone();
         Arrays.sort(copy);
-        int i=(nums.length-1)/2,j=nums.length-1,k=0;
-        while(k<nums.length){
+        int n=nums.length-1;
+        int i=(n)/2,j=n,k=0;
+        while(k<=n){
                 if(k%2==0){
                     nums[k++]=copy[i--];
                 }
