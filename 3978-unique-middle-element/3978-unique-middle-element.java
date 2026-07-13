9657fae65c390da45936e02d602f41b647cc10b4
sham_deshmukh_02 <141163978+shamdeshmukh22@users.noreply.github.com>
2026-07-14 00:25:18 +0530

    Time: 1 ms (94.98%), Space: 46.5 MB (52.62%) - LeetHub

--- 3978-unique-middle-element/3978-unique-middle-element.java
+++ 3978-unique-middle-element/3978-unique-middle-element.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean isMiddleElementUnique(int[] nums) {
-        int mid=nums[nums.length/2];
+        int mid=nums.length/2;
         for(int i=0;i<nums.length;i++){
-            if(i==nums.length/2)continue;
-            if(nums[i]==mid) return false;
+            if(nums[mid]==nums[i] && i!=mid)
+              return false;
         }
         return true;
     }
