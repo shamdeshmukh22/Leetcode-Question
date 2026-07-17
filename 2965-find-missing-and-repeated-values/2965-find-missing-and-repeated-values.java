03747b35cd230d88795352a330cad9b47a66751f
sham_deshmukh_02 <141163978+shamdeshmukh22@users.noreply.github.com>
2026-07-17 16:28:42 +0530

    Time: 1 ms (99.98%), Space: 47.6 MB (11.02%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
@@ -2,18 +2,17 @@ class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
         int n=grid.length,duplicate=-1;
         int arr[]=new int[n*n];
-        long totalSum=(n*n)*((n*n)+1)/2,currSum=0;
+        int mul=n*n;
+        long totalSum=mul*(mul+1)/2,currSum=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
             int val=grid[i][j]-1;
                if(arr[val]!=0)duplicate=val+1;
                else arr[val]+=1;
-               currSum+=grid[i][j];
+               currSum+=(val+1);
         }
     }
         currSum-=duplicate;
-        System.out.println(currSum+" "+totalSum);
-        int missing=(int)(totalSum-currSum);
-        return new int[]{duplicate,missing};
+        return new int[]{duplicate,(int)(totalSum-currSum)};
     }
 }
