e967fd6437a2eb467ffd793f5ab7daff87bdb438
sham_deshmukh_02 <141163978+shamdeshmukh22@users.noreply.github.com>
2026-07-19 12:29:22 +0530

    Time: 0 ms (100%), Space: 47.7 MB (18.43%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -17,27 +17,32 @@ class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
         if(root==null) return null;
         if(root.val<key){
-           root.right=deleteNode(root.right,key);
+            root.right=deleteNode(root.right,key);
         }
         else if(root.val>key){
             root.left=deleteNode(root.left,key);
         }
         else{
              if(root.left==null && root.right==null){
-                return null;
-            }
-            if(root.left!=null && root.right==null) return root.left;
-            if(root.right!=null  && root.left==null) return root.right;
+                      return null;
+             }
+            else if(root.left!=null && root.right==null){
+                      return root.left;
+             }
+             else if(root.left==null && root.right!=null){
+                      return root.right;
+             }
+             TreeNode min=FindMinNode(root.right);
+             root.val=min.val;
+             root.right=deleteNode(root.right,min.val);
+        } 
 
-            TreeNode min=FindMin(root.right);
-            root.val=min.val;
-            root.right=deleteNode(root.right,min.val);
-
-        }
         return root;
     }
-    public static TreeNode FindMin(TreeNode root){
-        while(root.left!=null)root=root.left;
+    public TreeNode FindMinNode(TreeNode root){
+        while(root.left!=null){
+            root=root.left;
+        }
         return root;
     }
 }
