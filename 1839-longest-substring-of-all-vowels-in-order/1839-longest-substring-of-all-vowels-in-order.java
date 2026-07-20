8182dab74f623ac6452d94ec69a3ad4fbfae5d53
sham_deshmukh_02 <141163978+shamdeshmukh22@users.noreply.github.com>
2026-07-21 00:45:19 +0530

    Time: 22 ms (61.16%), Space: 51.1 MB (25.22%) - LeetHub

--- 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.java
+++ 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.java
@@ -4,31 +4,30 @@ class Solution {
         char prev='0';
         while(i<word.length()){
             char ch=word.charAt(i);
-            if(prev==ch || CheckVowels(prev,ch) ){
-               if(ch=='u') max=Math.max(i-j+1,max);
-               prev=ch;
+            if(prev==ch || CheckPrevAndCurrent(prev,ch)){
+            if(ch=='u')max=Math.max(max,i-j+1);
+            prev=ch;
             }
             else{
-                if(ch=='a') {
-                    j=i;
+                if(ch=='a'){
                     prev='a';
+                    j=i;
                 }
-                else{
-                 j=i+1;
-                prev='0';
+                else {
+                    j=i+1;
+                    prev='0';
                 }
             }
             i++;
         }
         return max;
-
     }
-    public boolean CheckVowels(char prev,char next){
+    public boolean CheckPrevAndCurrent(char prev,char next){
         if(prev=='0' && next=='a') return true;
-       else if(prev=='a' && next=='e')return true;
-      else  if(prev=='e' && next=='i')return true;
-      else  if(prev=='i' && next=='o')return true;
-      else  if(prev=='o' && next=='u')return true;
+       else if(prev=='a' && next=='e') return true;
+       else if(prev=='e' && next=='i') return true;
+      else  if(prev=='i' && next=='o') return true;
+      else  if(prev=='o' && next=='u') return true;
       return false;
     }
 }
