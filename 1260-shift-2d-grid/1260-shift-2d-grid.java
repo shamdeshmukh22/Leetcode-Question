8b6d581fe92f269a7cf7335d5ddcdf0f9c7c0f02
sham_deshmukh_02 <141163978+shamdeshmukh22@users.noreply.github.com>
2026-07-20 19:49:42 +0530

    Time: 4 ms (97.01%), Space: 47.5 MB (8.97%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -4,27 +4,28 @@ class Solution {
         int n=m*r;
         if(k>=n) k%=n;
         List<List<Integer>>list=new ArrayList<>();
-        ArrayList<Integer>sub=new ArrayList<>();
         ArrayList<Integer>part=new ArrayList<>();
-       int i=0,j=0,l=k;
+
+        int arr[]=new int[n];
+       int i=0,j=0,idx=0;
         while(i<grid.length){
              j=0;
              while(j<grid[0].length){
-                  sub.add(grid[i][j]);  
+                   arr[idx++]=grid[i][j];
                 j++;
              }
              i++;
         }
         // list.add(sub);
         for(i=n-k;i<n;i++){
-            part.add(sub.get(i));
+            part.add(arr[i]);
             if(part.size()==r){
                 list.add(part);
                 part=new ArrayList<>();
             }
         }
         for( i=0;i<n-k;i++){
-            part.add(sub.get(i));
+            part.add(arr[i]);
             if(part.size()==r){
                 list.add(part);
                 part=new ArrayList<>();
