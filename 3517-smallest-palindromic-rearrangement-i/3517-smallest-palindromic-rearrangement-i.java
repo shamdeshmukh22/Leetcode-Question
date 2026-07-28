26a4ad318a3661907dc493bb0826b87daf5da8d5
sham_deshmukh_02 <141163978+shamdeshmukh22@users.noreply.github.com>
2026-07-28 09:54:13 +0530

    Time: 45 ms (12.45%), Space: 48.1 MB (57.43%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
@@ -5,22 +5,27 @@ class Solution {
         StringBuilder fr=new StringBuilder("");
         StringBuilder bk=new StringBuilder("");
         char mid='0';
+
         for(int i=0;i<str.length();i++){
             int idx=str.charAt(i)-'a';
             arr[idx]+=1;
         }
+
         for(int i=0;i<arr.length;i++){
+            char ch=(char)(97+i);
+
             while(arr[i]>1){
-                fr.append((char)(97+i));
-                bk.append((char)(97+i));
+                fr.append(ch);
+                bk.append(ch);
                 arr[i]-=2;
             }
             if(arr[i]%2!=0){
-                mid=(char)(97+i);
+                mid=ch;
             }
         }
         if(mid!='0')fr.append(mid);
         fr.append(bk.reverse());
+        
        return fr.toString();
     }
 }
