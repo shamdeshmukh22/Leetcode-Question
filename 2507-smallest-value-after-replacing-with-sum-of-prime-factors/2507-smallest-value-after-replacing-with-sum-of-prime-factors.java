86557b8cad169a68e75fd86ee59921e4d500ea62
sham_deshmukh_02 <141163978+shamdeshmukh22@users.noreply.github.com>
2026-07-31 19:27:39 +0530

    Time: 2 ms (37.93%), Space: 42.1 MB (73.89%) - LeetHub

--- 2507-smallest-value-after-replacing-with-sum-of-prime-factors/2507-smallest-value-after-replacing-with-sum-of-prime-factors.java
+++ 2507-smallest-value-after-replacing-with-sum-of-prime-factors/2507-smallest-value-after-replacing-with-sum-of-prime-factors.java
@@ -5,21 +5,21 @@ class Solution {
      private  int FactorOfN(int n,int prev) {
         if(prev==n) return n;
         prev=n;
-        int sum=0;
-        for(int i=2;i<=n;i++){
+        int sum=0,i=2;
+        while(i<=n){
             if(n%i==0 && checkPrime(i)) {
                 n/=i;
                 sum+=i;
-//                System.out.println(i);
                 i=1;
             }
+            i++;
         }
-//        System.out.println(prev+" "+sum);
         return FactorOfN(sum,prev);
     }
     public  boolean checkPrime(int n){
-        if(n==2) return true;
+        if(n==2 || n==3 || n==5 || n==7) return true;
         else if(n%2==0 ||  n<=1) return false;
+        
         for(int i=3;i*i<=n;i+=2){
             if(n%i==0) return false;
         }
