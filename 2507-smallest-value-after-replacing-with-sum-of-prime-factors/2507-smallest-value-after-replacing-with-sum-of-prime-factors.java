b44b01f61b9eb6399e1c286cc91377d4e912274e
sham_deshmukh_02 <141163978+shamdeshmukh22@users.noreply.github.com>
2026-07-31 19:28:50 +0530

    Time: 2 ms (37.93%), Space: 42.1 MB (61.58%) - LeetHub

--- 2507-smallest-value-after-replacing-with-sum-of-prime-factors/2507-smallest-value-after-replacing-with-sum-of-prime-factors.java
+++ 2507-smallest-value-after-replacing-with-sum-of-prime-factors/2507-smallest-value-after-replacing-with-sum-of-prime-factors.java
@@ -7,10 +7,9 @@ class Solution {
         prev=n;
         int sum=0,i=2;
         while(i<=n){
-            if(n%i==0 && checkPrime(i)) {
+            while(n%i==0 && checkPrime(i)) {
                 n/=i;
                 sum+=i;
-                i=1;
             }
             i++;
         }
@@ -19,7 +18,7 @@ class Solution {
     public  boolean checkPrime(int n){
         if(n==2 || n==3 || n==5 || n==7) return true;
         else if(n%2==0 ||  n<=1) return false;
-        
+
         for(int i=3;i*i<=n;i+=2){
             if(n%i==0) return false;
         }
