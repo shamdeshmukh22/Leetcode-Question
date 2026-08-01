eb0fe96ad24a23ca531e9a7b8497166908b2a49d
sham_deshmukh_02 <141163978+shamdeshmukh22@users.noreply.github.com>
2026-08-01 11:04:58 +0530

    Time: 11 ms (40.41%), Space: 47.5 MB (47.07%) - LeetHub

--- 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.java
+++ 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.java
@@ -3,12 +3,13 @@ class Solution {
         HashSet<Integer>set=new HashSet<>();
 
         for(int n:arr){
-             for(int i=2;i<=n;i++){
+             for(int i=2;i*i<=n;i++){
                 while(n%i==0){
                     n/=i;
                     set.add(i);
                 }
              }
+             if(n>1)set.add(n);
         }
         return set.size();
     }
