615f830b7d97fea1050e114ae50e5c9ba84018f1
sham_deshmukh_02 <141163978+shamdeshmukh22@users.noreply.github.com>
2026-08-07 21:11:15 +0530

    Time: 16 ms (36.4%), Space: 46.6 MB (52.24%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,7 +1,7 @@
-class CountCharFreq{
+class CountFreq{
     char ch;
     int freq;
-    CountCharFreq(char ch,int freq){
+    CountFreq(char ch,int freq){
         this.ch=ch;
         this.freq=freq;
     }
@@ -9,28 +9,27 @@ class CountCharFreq{
 
 class Solution {
     public String frequencySort(String s) {
-        HashMap<Character,CountCharFreq>map=new HashMap<>();
-
+        HashMap<Character,CountFreq>map=new HashMap<>();
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
-              if(map.containsKey(ch)){
-                  CountCharFreq c= map.get(ch);
-                  c.freq+=1;
-              }
-              else map.put(ch,new CountCharFreq(ch,1));
+            if(map.containsKey(ch)){
+                CountFreq c=map.get(ch);
+                c.freq+=1;
+            }
+            else {
+                map.put(ch,new CountFreq(ch,1));
+            }
         }
-        
-        StringBuilder sb=new StringBuilder("");
-        ArrayList<CountCharFreq>list=new ArrayList<>();
-              
+        ArrayList<CountFreq>list=new ArrayList<>();
+       StringBuilder sb=new StringBuilder("");
+
         map.forEach((a,b)->list.add(b));
         Collections.sort(list,(a,b)->Integer.compare(b.freq,a.freq));
-
+          
         for(int i=0;i<list.size();i++){
-            CountCharFreq c=list.get(i);
+            CountFreq c=list.get(i);
             sb.repeat(c.ch,c.freq);
         }
-
         return sb.toString();
     }
 }
