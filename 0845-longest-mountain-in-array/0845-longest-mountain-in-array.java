fc6dadf1af793d7b4e73fc86633c7f72f1e17b7f
sham_deshmukh_02 <141163978+shamdeshmukh22@users.noreply.github.com>
2026-08-12 19:10:29 +0530

    Time: 101 ms (5.02%), Space: 47.7 MB (7.13%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.java
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.java
@@ -3,6 +3,7 @@ class Solution {
         int max=0;
         int i=0;
         while(i<arr.length){
+            
             int left=i,right=i;
              while(left>0 && arr[left] >arr[left-1]){
                 left--;
@@ -10,7 +11,10 @@ class Solution {
              while(right<arr.length-1 && arr[right]>arr[right+1]){
                 right++;
              }
-            if(left!=i && right!=i) max=Math.max(max,right-left+1);
+            if(left!=i && right!=i) {
+                max=Math.max(max,right-left+1);
+                i=right-1;
+             }
              i++;
         }
         return max;
