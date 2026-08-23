f087cecbbad60e69be5062cd41bd3f59a664bdf7
sham_deshmukh_02 <141163978+shamdeshmukh22@users.noreply.github.com>
2026-08-23 21:31:19 +0530

    Time: 6 ms (87.17%), Space: 46.8 MB (5.72%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -7,9 +7,10 @@ class Solution {
         for(String s:tokens){
             int idx=CheckIndex(s);
               if(idx!=-1){
-                   if(st.size()>1){
+                    
                     int second=Integer.parseInt(st.pop());
                     int first=Integer.parseInt(st.pop());
+
                     if(idx==0){
                         st.push(String.valueOf(first+second));
                     }
@@ -22,12 +23,11 @@ class Solution {
                     else{
                         st.push(String.valueOf(first/second));
                     }
-                   }
               }
               else{
                 st.push(s);
               }
         }
-        return Integer.valueOf(st.pop());
+         return Integer.valueOf(st.pop());
     }
 }
